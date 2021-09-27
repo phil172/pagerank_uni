980573ad739921bde48034d96a2e1a2d37bcfe37
Philipp <philipp@KubaToshs-iMac.local>
2021-09-27 14:47:48 +0200

    indize gefixt

--- code/json_to_graph.py
+++ code/json_to_graph.py
@@ -26,22 +26,18 @@ class Graph():
     def get_graphs(self):
         ids: dict = self.ids
         data: dict = self.data
-        for i in range(1, len(list(data.keys()))):
+        print(len(list(data.keys())))
+        for i in range(0, len(list(data.keys()))):
             lst = list(data[list(data.keys())[i]])
             new_lst = []
             A = (pd.Series(lst)).map(ids) #convert the list to a pandas series temporarily before mapping
             B = list(A.dropna())
             for el in B:
                 new_lst.append(int(el))
-            self.graph[i] = new_lst
+            self.graph[i+1] = new_lst
         return self.graph
 
 
-    def test(self):
-        ids: dict = self.ids
-        data: dict = self.data
-        print(ids)
-        lst = list(data[list(data.keys())[0]])
 
 gr = Graph(idpath, datapath)
 graph = gr.get_graphs()
--- code/searchhtml.py
+++ code/searchhtml.py
@@ -1,30 +1,37 @@
 from bs4 import BeautifulSoup
 import ssl
+from urllib.request import urlopen
 base_url = "https://www.math.kit.edu/"
 ssl._create_default_https_context = ssl._create_unverified_context
 
-### Nur Text importieren
+class HtmlImport():
+    def __inti__(self):
+        self.text = self.getText()
+        self.soup = self.getSoup()
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+    def getSoup(self):
+        with open("pages/1.html") as html_file:
+            soup = BeautifulSoup(html_file, features="html.parser")
+            return soup
+    # url = base_url
+    # html = urlopen(url).read()
+    # soup = BeautifulSoup(html, features="html.parser")
 
-url = base_url
-html = urlopen(url).read()
-soup = BeautifulSoup(html, features="html.parser")
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+        
+    def getText(self):
+        soup = self.soup
+        # get text
+        for script in soup(["script", "style"]):
+            script.extract()    # rip it out
+        text = soup.get_text()
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
-
-# get text
-text = soup.get_text()
-
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-a = text.find("Mathematik")
-print(text[a:])
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = '\n'.join(chunk for chunk in chunks if chunk)
+        return text
