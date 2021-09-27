133a259184d1875d3eb840a46432987a3cc89e6f
Philipp <philipp@KubaToshs-iMac.local>
2021-09-27 15:45:03 +0200

    merge

--- code/searchhtml.py
+++ code/searchhtml.py
@@ -1,28 +1,27 @@
 from bs4 import BeautifulSoup
 import ssl
 from urllib.request import urlopen
+import re, os
+import json
 base_url = "https://www.math.kit.edu/"
 ssl._create_default_https_context = ssl._create_unverified_context
 
 class HtmlImport():
-    def __inti__(self):
-        self.text = self.getText()
-        self.soup = self.getSoup()
+    def __init__(self, path):
+        self.path = path
+        self.soup = self.get_soup()
+        self.text = self.get_text()
+        
 
-    def getSoup(self):
-        with open("pages/1.html") as html_file:
+    def get_soup(self):
+        with open(self.path) as html_file:
             soup = BeautifulSoup(html_file, features="html.parser")
             return soup
-    # url = base_url
-    # html = urlopen(url).read()
-    # soup = BeautifulSoup(html, features="html.parser")
 
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-        
-    def getText(self):
+    def get_text(self):
         soup = self.soup
+        for script in self.soup(["script", "style"]):
+            script.extract()    # rip it out
         # get text
         for script in soup(["script", "style"]):
             script.extract()    # rip it out
@@ -35,3 +34,26 @@ class HtmlImport():
         # drop blank lines
         text = '\n'.join(chunk for chunk in chunks if chunk)
         return text
+
+def to_json(dct, file):
+    with open(file, "w") as outfile:
+        json.dump(dct, outfile)
+    
+all_pages = os.listdir("pages")
+
+
+searchterm = input("Search something...")
+searchterm = searchterm.lower()
+search_dict = dict()
+
+for page in all_pages:
+    h = HtmlImport("pages/"+page)
+    text: str = h.get_text()
+    text = text.lower()
+    pos = re.finditer(searchterm, text)
+    spans = []
+    for el in pos:
+        spans.append(el.span())
+        search_dict[page] = spans
+
+to_json(search_dict, "searchdict.json")
